d3c1eaa86754fdd38ca009250479867829304b01
raphnw <120077030+raphnw@users.noreply.github.com>
2023-02-08 16:55:29 -0700

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://44.200.159.3:9000/',
+  serverUrl: 'http://54.197.205.8:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': '1afd6d7e211c0f5de8ca7429f75e8f0a9c789c88',
+	    'sonar.login': '006040d0315d593dd5335c2bb57b0576969e8a86',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
